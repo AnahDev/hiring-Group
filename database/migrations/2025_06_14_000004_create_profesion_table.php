57212aaa9b9a4fe30036dcd1c4dda75f30d65c87
DavFer023 <davferez023@gmail.com>
2025-06-13 23:14:38 -0400

    Agregar migraciones para las tablas usuario, profesion, banco, candidato, empresa, contrato, nomina, oferta_laboral y postulacion. Para seguir el orden

--- database/migrations/2025_06_14_000004_create_profesion_table.php
+++ database/migrations/2025_06_14_000004_create_profesion_table.php
@@ -30,6 +30,6 @@ return new class extends Migration
     public function down(): void
     {
         Schema::dropIfExists('profesion');
-        Schema::dropIfExists('candidato_Profesion');
+        //Schema::dropIfExists('candidato_Profesion');
     }
 };
