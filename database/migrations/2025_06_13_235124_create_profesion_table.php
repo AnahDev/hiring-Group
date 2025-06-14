45aa8f5c9ebcebd326198bf054a24ee49c2480ed
DavFer023 <davferez023@gmail.com>
2025-06-13 21:12:16 -0400

    falto un rollback

--- database/migrations/2025_06_13_235124_create_profesion_table.php
+++ database/migrations/2025_06_13_235124_create_profesion_table.php
@@ -30,5 +30,6 @@ return new class extends Migration
     public function down(): void
     {
         Schema::dropIfExists('profesion');
+        Schema::dropIfExists('candidato_Profesion');
     }
 };
